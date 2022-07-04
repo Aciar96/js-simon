cdce16fecf2a373930b74c6477fc392e7dcfebdd
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-07-04 14:48:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//recupero dal dom gli elementi utili
+//genero 5 numeri random da inserire nel <div id="random"> 
+//mostro sullo schermo per un tempo limitato i numeri dopodiche rendo "none" i numeri 
+//chiedo all' utente per 5 volte di inserire i numeri da 1-100
+//dico all'utente con un allert quanti numeri ha memorizzato 
+
+
+//recupero dal dom gli elementi utili
+const dysplay = document.getElementById('display')
+let randomNuber = document.getElementById('random')
+
+//genero 5 numeri random da inderire nel <div id="random"> 
+
+//devifinisco i valori min e max
+let min = 1; 
+let max = 99;
+
+//genero 5 numeri random
+for(let i = 0; i < 5 ; i++){
+ randomNuber = Math.floor(Math.random() *(max-min)+min);
+ console.log(randomNuber)
+}
+
+randomNuber.innerText = randomNuber;
+
+
+
+
+
+
+
+
