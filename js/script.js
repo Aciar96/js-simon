f72d6b2f9e6fba55e63159a0104539a32bfe7c82
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-07-04 17:38:13 +0200

    haswon

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
 //2-genero 5 numeri random da inserire nel <div id="random"> 
 //3-mostro sullo schermo per un tempo limitato i numeri dopodiche rendo "none" i numeri 
 //4-chiedo all' utente per 5 volte di inserire i numeri da 1-99
-//5-dico all'utente con un allert quanti numeri ha memorizzato 
+//5-comparo l'userNumber con i numeri presenti in arrey 
+//6-dico all'utente con un allert quanti numeri ha memorizzato 
 /////////////funzione///////////
-function random(min,max){
-    for(let i = 0; i < 5 ; i++){
-        random  = Math.floor(Math.random() *(max-min)+min);
+function random(min, max) {
+    for (let i = 0; i < 5; i++) {
+        random = Math.floor(Math.random() * (max - min) + min);
         console.log(random)
         //pusho il mio numero random nell arrey 
         num.push(random)
@@ -23,20 +24,46 @@ let randomNuber = document.getElementById('random')
 let num = []
 console.log(num)
 
+//preparo un arrey per contenere i 5 numeri dell'utente
+
 //2-genero 5 numeri random da inserire nel <div id="random"> 
 
 //devifinisco i valori min e max
-const min = 1; 
+const min = 1;
 const max = 99;
 //genero 5 numeri random
 
-random(min,max)
+random(min, max)
 
-//3-mostro sullo schermo per un tempo limitato i numeri dopodiche rendo "none" i numeri 
-const timer = setTimeout(function(){
+//3-mostro sullo schermo per un tempo limitato i numeri dopodiche rendo 
+const timer = setTimeout(function () {
     randomNuber.innerText = '';
-},1000)
-console.log(timer)
+}, 5000)
+
+//4-chiedo all' utente per 5 volte di inserire i numeri da 1-99
+//creo una variabile d'appoggio per inserire il risultato 
+let result;
+let quest
+
+const userNuber = setTimeout(function () {
+    for (let i = 0; i <= 4; i++) {
+         quest = parseInt(prompt(`inserisci un numero da ${min} a ${max}`))
+        console.log(quest[i])
+    }
+    if (quest === randomNuber) { 
+        result = 5
+    } else{
+         myAlert = alert("Hai indovinato" + result[i] + "numeri")
+
+    }
+
+    
+
+   
+
+}, 5100)
+
+
 
 
 
