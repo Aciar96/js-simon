9d8bb98997f986e0347818c3a307cfddda3622a9
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-07-04 15:08:43 +0200

    random number

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-//recupero dal dom gli elementi utili
-//genero 5 numeri random da inserire nel <div id="random"> 
+//1-recupero dal dom gli elementi utili
+//2-genero 5 numeri random da inserire nel <div id="random"> 
 //mostro sullo schermo per un tempo limitato i numeri dopodiche rendo "none" i numeri 
 //chiedo all' utente per 5 volte di inserire i numeri da 1-100
 //dico all'utente con un allert quanti numeri ha memorizzato 
 
 
-//recupero dal dom gli elementi utili
+//1-recupero dal dom gli elementi utili
 const dysplay = document.getElementById('display')
 let randomNuber = document.getElementById('random')
 
-//genero 5 numeri random da inderire nel <div id="random"> 
+//preparo un arrey per contenere i 5 numeri random
+let num = []
+console.log(num)
+
+//2-genero 5 numeri random da inderire nel <div id="random"> 
 
 //devifinisco i valori min e max
 let min = 1; 
@@ -19,9 +23,15 @@ let max = 99;
 for(let i = 0; i < 5 ; i++){
  randomNuber = Math.floor(Math.random() *(max-min)+min);
  console.log(randomNuber)
+ //pusho il mio numero random nell arrey 
+num.push(randomNuber)
+console.log(num)
 }
 
-randomNuber.innerText = randomNuber;
+//publico in pagina i 5 numeri random
+randomNuber
+
+
 
 
 
