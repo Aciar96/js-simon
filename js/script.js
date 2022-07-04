02420d8377ba2f08ec1bbfeb8fb022382b138345
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-07-04 16:25:01 +0200

    fixed settimout

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //1-recupero dal dom gli elementi utili
 //2-genero 5 numeri random da inserire nel <div id="random"> 
 //3-mostro sullo schermo per un tempo limitato i numeri dopodiche rendo "none" i numeri 
-//4-chiedo all' utente per 5 volte di inserire i numeri da 1-100
+//4-chiedo all' utente per 5 volte di inserire i numeri da 1-99
 //5-dico all'utente con un allert quanti numeri ha memorizzato 
 /////////////funzione///////////
-function random(){
+function random(min,max){
     for(let i = 0; i < 5 ; i++){
         random  = Math.floor(Math.random() *(max-min)+min);
         console.log(random)
@@ -26,14 +26,16 @@ console.log(num)
 //2-genero 5 numeri random da inserire nel <div id="random"> 
 
 //devifinisco i valori min e max
-let min = 1; 
-let max = 99;
+const min = 1; 
+const max = 99;
 //genero 5 numeri random
 
 random(min,max)
 
 //3-mostro sullo schermo per un tempo limitato i numeri dopodiche rendo "none" i numeri 
-const timer = setTimeout(random ,10000)
+const timer = setTimeout(function(){
+    randomNuber.innerText = '';
+},1000)
 console.log(timer)
 
 
